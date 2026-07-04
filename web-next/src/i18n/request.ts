9e8804ab87359ee7cd6ee71b1462c1169ca189bd
t4we fan <guanghoushi@gmail.com>
2026-07-04 23:53:20 +0800

    Track locale message imports explicitly

--- web-next/src/i18n/request.ts
+++ web-next/src/i18n/request.ts
@@ -2,6 +2,11 @@ import { getRequestConfig } from "next-intl/server";
 import { hasLocale } from "next-intl";
 import { routing } from "./routing";
 
+const messagesByLocale = {
+  en: () => import("../../messages/en.json"),
+  "zh-CN": () => import("../../messages/zh-CN.json")
+} satisfies Record<(typeof routing.locales)[number], () => Promise<{ default: unknown }>>;
+
 export default getRequestConfig(async ({ requestLocale }) => {
   const requested = await requestLocale;
   const locale = hasLocale(routing.locales, requested)
@@ -10,6 +15,6 @@ export default getRequestConfig(async ({ requestLocale }) => {
 
   return {
     locale,
-    messages: (await import(`../../messages/${locale}.json`)).default
+    messages: (await messagesByLocale[locale]()).default
   };
 });
